bcbd9ac761d613f10f53ba3ceee614f1dc6c7005
txmiaomiaowu <mx20220529@minxinnansha.org.cn>
2023-08-19 12:38:31 +0800

    114514

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ rm -rf source/_posts
 mkdir -p source/_posts
 
 # 安装主题(你们可以改成自己喜欢的主题)
-git clone https://github.com/theme-next/hexo-theme-next themes/next
+# git clone https://github.com/theme-next/hexo-theme-next themes/next
 
 # 使用新的渲染器
 npm un hexo-renderer-marked --save
