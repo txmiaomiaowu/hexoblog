8e7ff210e43d394327054f963d75b022830c1bc7
txmiaomiaowu <mx20220529@minxinnansha.org.cn>
2023-08-20 10:27:10 +0800

    new changes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf ./publish
 npm install -g hexo-cli
 
 #安装插件
-
+npm clean-install --progress=false -f
 npm remove hexo-renderer-marked --save
 npm add hexo-renderer-markdown-it hexo-tag-mermaid --save
 npm install markdown-it-merge-cells --save
@@ -26,20 +26,20 @@ npm install hexo-pinyin-ruby-marks -- save
 npm install hexo-spoiler --save
 npm install hexo-tag-common --save
 npm install hexo-tag-chart --save
-npm install hexo-pangu --save
-npm install hexo-tag-hint --save
-npm i hexo-tag-qrcode --save
-npm install @heowc/hexo-tag-gdemo --save
-npm install hexo-github-card --save
-npm install hexo-auto-category --save
-npm install hexo-tag-plantuml --save
-npm install hexo-markmap --save
-npm install hexo-matery-url --save
-npm install hexo-lazyload-image --save
+npm install hexo-pangu --save -f
+npm install hexo-tag-hint --save -f
+npm i hexo-tag-qrcode --save -f
+npm install @heowc/hexo-tag-gdemo --save -f
+npm install hexo-github-card --save -f
+npm install hexo-auto-category --save -f
+npm install hexo-tag-plantuml --save -f
+npm install hexo-markmap --save -f
+npm install hexo-matery-url --save -f
+npm install hexo-lazyload-image --save -f
 npm install hexo-filter-lqip --save -f
 npm install gulp -g --save -f
 npm install gulp gulp-htmlclean gulp-htmlmin gulp-minify-css gulp-uglify --save -f
-npm install gulp-debug gulp-clean-css gulp-changed gulp-if gulp-plumber gulp-babel babel-preset-es2015 del @babel/core --save -f
+npm install gulp-debug gulp-clean-css gulp-changed gulp-if gulp-plumber gulp-babel babel-preset-es2015 @babel/core --save -f
 npm install gulp-imagemin@7.1.0 --save -f
 #尝试优化
 
